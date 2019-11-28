6e4294d3a24d4c18134f62cfe969d162d66b43ea
billonline33 <billonline33@hotmail.com>
2019-11-28 23:21:36 +1100

    enable devToolsExtension

--- src/_helpers/store.js
+++ src/_helpers/store.js
@@ -1,14 +1,25 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../_reducers';
 
+const enhancers=[];
 const loggerMiddleware = createLogger();
+
+if (process.env.NODE_ENV === "development") {
+    const devToolsExtension = window.devToolsExtension;
+  
+    if (typeof devToolsExtension === "function") {
+      enhancers.push(devToolsExtension());
+    }
+}
+  
+const composeEnhancers=compose( applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+    ), ...enhancers);
 
 export const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    composeEnhancers
 );
